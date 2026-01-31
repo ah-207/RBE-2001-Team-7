08c79e39d9ad20e821b80d751ea76f5836d1f6b8
Andrea <98416463+andreatravagliante@users.noreply.github.com>
2026-01-31 16:36:45 -0500

    Add files via upload

---
+++ 2001-lab2/src/robot-nav.cpp
@@ -0,0 +1,90 @@
+/**
+ * robot-nav.cpp is where you should put navigation routines.
+ */
+
+#include "robot.h"
+
+void Robot::UpdatePose(const Twist& twist) //estimates where robot is
+{
+    
+    const float CONTROL_LOOP_PERIOD_MS = 10;
+    float dt = CONTROL_LOOP_PERIOD_MS / 1000.0f;
+    
+
+   // Update heading
+    currPose.theta += twist.omega * dt;
+
+    // Update position
+    currPose.x += twist.u * cos(currPose.theta) * dt;
+    currPose.y += twist.u * sin(currPose.theta) * dt;
+
+
+#ifdef __NAV_DEBUG__
+    TeleplotPrint("x", currPose.x);
+    TeleplotPrint("y", currPose.y);
+    TeleplotPrint("theta", currPose.theta);
+#endif
+
+
+}
+
+/**
+ * Sets a destination in the lab frame.
+ */
+void Robot::SetDestination(const Pose& dest)
+{
+    /**
+     * TODO: Turn on LED, as well.
+     */
+    Serial.print("Setting dest to: ");
+    Serial.print(dest.x);
+    Serial.print(", ");
+    Serial.print(dest.y);
+    Serial.print('\n');
+
+    startPose = currPose; //says that the current pose is where the robot started moving
+    destPose = dest;
+    robotState = ROBOT_DRIVE_TO_POINT;
+}
+
+bool Robot::CheckReachedDestination(void) //checks if distance >= 60cm
+{
+    bool retVal = false;
+    float dx = currPose.x - startPose.x;
+    float dy = currPose.y - startPose.y;
+    float dist = sqrt(dx*dx + dy*dy);
+
+    if(dist >= 60.0)
+    {
+        return retVal;
+    }
+
+    
+}
+
+void Robot::DriveToPoint(void) //tells robot to go straight
+{
+    if(robotState == ROBOT_DRIVE_TO_POINT)
+    {
+        float effort = 50;
+        chassis.SetMotorEfforts(effort, effort); //same effort for both wheels
+#ifdef __NAV_DEBUG__
+        float dx = currPose.x - startPose.x;
+        float dy = currPose.y - startPose.y;
+        float dist = sqrt(dx*dx + dy*dy);
+
+        TeleplotPrint("dist", dist);
+        TeleplotPrint("x", currPose.x);
+        TeleplotPrint("y", currPose.y);
+        TeleplotPrint("theta", currPose.theta);
+#endif
+
+       
+    }
+}
+
+void Robot::HandleDestination(void) //stops robot
+{
+    chassis.SetMotorEfforts(0, 0); //stopped motors
+    robotState = ROBOT_IDLE; //changed state to idle
+}
